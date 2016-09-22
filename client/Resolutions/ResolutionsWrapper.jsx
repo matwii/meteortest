4c110cfb4c46a1c7f6a88ba6637b664d22ff2ab9
Matz Wiik <matzwiik@gmail.com>
2016-09-22 22:43:43 +0200

    video 9 on tutorial

---
+++ client/Resolutions/ResolutionsWrapper.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TrackerReact from 'meteor/ultimatejs:tracker-react';
+import ResolutionsForm from './ResolutionsForm.jsx';
+import ResolutionSingle from './ResolutionSingle.jsx';
+
+Resolutions = new Mongo.Collection("resolutions");
+
+export default class ResolutionsWrapper extends TrackerReact(React.Component) {
+
+    resolutions(){
+        return Resolutions.find().fetch();//Fetch gives us an object
+    }
+
+    render(){
+        console.log(this.resolutions());
+        return(
+            <div>
+                <h1>My Resolutions</h1>
+                <ResolutionsForm />
+                <ul className="resolutions">
+                    {this.resolutions().map((resolution) => {
+                        return <ResolutionSingle key={resolution._id} resolution={resolution}/>
+                    })}
+                </ul>
+           </div>
+        )
+    }
+}
+
--- client/Routes.jsx
+++ client/Routes.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import {mount} from 'react-mounter';
 import {MainLayout} from './layouts/MainLayout.jsx';
-import App from './App.jsx';
+import ResolutionsWrapper from './Resolutions/ResolutionsWrapper.jsx';
 
 FlowRouter.route('/',{
     //What happens when you are getting routed
     action(){
         mount(MainLayout, {
-            content: (<App />)
+            content: (<ResolutionsWrapper />)
         })
     }
 });
